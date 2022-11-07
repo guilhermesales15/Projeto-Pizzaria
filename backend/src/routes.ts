a3cde38268ea54134a778870253d4aba5a8d2144
Guilherme Sales <guilhermefeit4@gmail.com>
2022-11-07 18:46:05 -0300

    Criando middlewares no processo de validar o login

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,10 +2,15 @@ import { Router,Request, Response } from "express";
 
 import {CreateUserController} from './controller/user/CreateUserController';
 import { LoginUserController } from "./controller/user/LoginUserController";
+import {DetailUserController} from "./controller/user/DetailUserController";
+import { Authenticated } from "./middlewares/authenticated";
 
 const router = Router();
 //Rota User
 router.post('/users',new CreateUserController().handle);
 router.post('/session', new LoginUserController().handle);
+
+router.get('/detail',Authenticated, new DetailUserController().handle);
+
 
 export { router};
