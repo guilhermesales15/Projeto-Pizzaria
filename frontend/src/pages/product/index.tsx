ed5cf6d83db3e54af44e04576365ce644f0d1097
Guilherme Sales <guilhermefeit4@gmail.com>
2023-01-10 15:25:18 -0300

    criando o preview da imagem do item

--- frontend/src/pages/product/index.tsx
+++ frontend/src/pages/product/index.tsx
@@ -1,41 +1,116 @@
-import Head from "next/head";
-import styles from './style.module.scss';
-import { canSSRGuest } from "../../utils/canSSRguest";
-import { Header } from "../../components/ui/Header";
+import { useState, ChangeEvent } from 'react'
+import Head from 'next/head';
+import styles from './style.module.scss'
+import { Header } from '../../components/ui/Header';
+
+import { canSSRauth } from '../../utils/canSSRauth';
+
+import { FiUpload } from 'react-icons/fi'
+
 
 export default function Product(){
-    return(
-        <>
-        <Head>
-            <title>Novo Produto - PizzariaJs</title>
-        </Head>
-        <div>
-            <Header/>
-            <main className={styles.container}>
-                <h1>Novo Produto</h1>
-                <form className={styles.form}>
-                    <select>
-                        <option>
-                            bebida
-                        </option>
-                    </select>
-                    <input className={styles.input} type="text" placeholder="digite o nome do produto"/>
-                    <input className={styles.input} type="text" placeholder="Preço do produto" />
-                    <textarea className={styles.input} placeholder="Descrição do produto"/>
-
-                    <button className={styles.buttonAdd} type="submit">Cadastrar</button>
-                </form>
-            </main>
-        </div>
-        
-        </>
-    )
 
 
+  const [avatarUrl, setAvatarUrl] = useState('');
+  const [imageAvatar, setImageAvatar] = useState(null);
+
+
+  function handleFile(e: ChangeEvent<HTMLInputElement>){
+
+    if(!e.target.files){
+      return;
+    }
+
+    const image = e.target.files[0];
+
+    if(!image){
+      return;
+    }
+
+    if(image.type === 'image/jpeg' || image.type === 'image/png'){
+
+      setImageAvatar(image);
+      setAvatarUrl(URL.createObjectURL(e.target.files[0]))
+
+    }
+
+  }
+
+  return(
+    <>
+      <Head>
+        <title>Novo produto - Sujeito Pizzaria</title>
+      </Head>
+      <div>
+        <Header/>
+
+        <main className={styles.container}>
+          <h1>Novo produto</h1>
+
+          <form className={styles.form}>
+
+            <label className={styles.labelAvatar}>
+              <span>
+                <FiUpload size={30} color="#FFF" />
+              </span>
+
+              <input type="file" accept="image/png, image/jpeg" onChange={handleFile} />
+
+              {avatarUrl && (     
+                  <img 
+                    className={styles.preview}
+                    src={avatarUrl}
+                    alt="Foto do produto" 
+                    width={250}
+                    height={250}
+                  />
+              )}
+
+            </label>
+
+
+            <select>
+              <option>
+                Bebida
+              </option>
+              <option>
+                Pizzas
+              </option>
+            </select>
+
+            <input 
+            type="text"
+            placeholder="Digite o nome do produto"
+            className={styles.input}
+            />
+
+            <input 
+            type="text"
+            placeholder="Preço do produto"
+            className={styles.input}
+            />      
+
+            <textarea 
+              placeholder="Descreva seu produto..."
+              className={styles.input}
+            /> 
+
+            <button className={styles.buttonAdd} type="submit">
+              Cadastrar  
+            </button>   
+
+          </form>
+
+        </main>
+
+      </div>
+    </>
+  )
 }
 
-export const getServerSideProps = canSSRGuest(async(ctx)=>{
-    return{
-        props:{}
-}
+export const getServerSideProps = canSSRauth(async (ctx) => {
+
+  return {
+    props: {}
+  }
 })
